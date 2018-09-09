72e540514a11fd564f857bbc8887244b3958b38e
Derek Middlemiss <derekmiddlemiss@gmail.com>
2018-09-09 16:32:30 +0100

    Can remove item from basket. Attempt to remove non-existent item from basket yields warning

--- Basket.py
+++ Basket.py
@@ -8,5 +8,11 @@ class Basket:
 
 	def add_item(self, item):
 		self.items.append(item)
-	
+
+	def remove_item(self, item):
+		try:
+			self.items.remove(item)
+		except ValueError:
+			return "Item not in basket"
+
 
--- test_Basket.py
+++ test_Basket.py
@@ -2,21 +2,26 @@ import pytest
 from Basket import Basket
 from Item import Item
 
-@pytest.fixture(scope="module")
-def empty_basket():
-	return Basket()
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def book():
 	return Item("Of Mice and Men", "Paperback, Of Mice and Men, Steinbeck", "book_OMaM_01", 8.99)
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def dvd():
 	return Item("Fellowship of the Ring", "Lord of the Rings, Fellowship of the Ring", "DVD_FotR_1", 9.99)
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
+def empty_basket():
+	return Basket()
+
+@pytest.fixture(scope="function")
 def basket_one_item(book):
 	items = [book]
+	return Basket(items)
+
+@pytest.fixture(scope="function")
+def basket_two_items(book, dvd):
+	items = [book, dvd]
 	return Basket(items)
 
 def test_can_instantiate_basket(empty_basket):
@@ -32,6 +37,18 @@ def test_empty_basket_can_add_item(empty_basket, book):
 	empty_basket.add_item(book)
 	assert empty_basket.is_item_in_basket(book)
 
-def test_basket_with_one_item_can_add_another(basket_one_item, dvd):
+def test_basket_with_one_item_can_add_another(basket_one_item, book, dvd):
 	basket_one_item.add_item(dvd)
 	assert basket_one_item.is_item_in_basket(dvd)
+	assert basket_one_item.is_item_in_basket(book)
+
+def test_remove_item_in_basket(basket_one_item, book):
+	basket_one_item.remove_item(book)
+	assert basket_one_item.is_item_in_basket(book) == False
+
+def test_remove_item_not_in_basket(basket_one_item, dvd):
+	return_message = basket_one_item.remove_item(dvd)
+	assert return_message == "Item not in basket"
+
+
+
